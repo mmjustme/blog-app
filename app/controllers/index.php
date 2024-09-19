2d12ca25c3eb0e3bcc12b7cc84eec264e7529133
mmjustme <infooneuser@gmail.com>
2024-09-19 21:23:58 +0300

    Small update DB

--- app/controllers/index.php
+++ app/controllers/index.php
@@ -2,7 +2,7 @@
 
 
 $posts = $db->query("SELECT * FROM posts")->findAll();
-$recent_posts = $db->query("SELECT * FROM posts ORDER BY created_at LIMIT 5")->findAll();
+$recent_posts = $db->query("SELECT * FROM posts ORDER BY id LIMIT 5")->findAll();
 // dd($posts);
 
 require VIEWS . "/index.tpl.php";
