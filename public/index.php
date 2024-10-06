d359d30baaf9d9c033890ea9e38d9cc2bd4ad45a
mmjustme <infooneuser@gmail.com>
2024-10-06 15:40:19 +0300

    1. BUG>index.php forgot include autoloader!!!

--- public/index.php
+++ public/index.php
@@ -4,10 +4,14 @@ ini_set('display_errors', 1);
 
 require dirname(__DIR__) . "/config/config.php";
 require CORE . "/funcs.php";
-require CORE . "/classes/DB.php";
+require CORE . '/../autoload.php';
 
 $db_config = require CONFIG . "/db.php";
 $db = \core\Db::getInstance()->getConnection($db_config);
 
+// $router = new \core\Router();
+// dd($router->routes);
+
+require CONFIG . '/routes.php';
 
 require CORE . "/router.php";
