948bc3b2ea6f5f36348ea20d94ec393e3185c1e7
mmjustme <infooneuser@gmail.com>
2024-11-19 20:36:48 +0200

    Small css update.

--- app/views/inc/header.inc.php
+++ app/views/inc/header.inc.php
@@ -29,14 +29,14 @@
 					<div class="collapse navbar-collapse" id="navbarSupportedContent">
 						<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 							<li class="nav-item">
-								<a class="nav-link active" aria-current="page" href="">Home</a>
+								<a class="nav-link active" aria-current="page" href="">HOME</a>
 							</li>
 							<li class="nav-item">
-								<a class="nav-link" href="posts/create">New Post</a>
+								<a class="nav-link" href="posts/create">NEW POST</a>
 							</li>
 							<?php if (checkAuth()): ?>
 								<li class="nav-item">
-									<a class="nav-link" href="users/posts">My Posts</a>
+									<a class="nav-link" href="users/posts">MY POSTS</a>
 								</li>
 							<?php endif ?>
 						</ul>
--- app/views/posts/index.tpl.php
+++ app/views/posts/index.tpl.php
@@ -3,7 +3,7 @@
 <main class="main">
 	<div class="container">
 		<div class="row">
-			<div class="col-8">
+			<div class="col-12 col-sm-8">
 				<?php foreach ($posts as $post): ?>
 					<div class="card mb-3">
 						<div class="card-body">
@@ -15,17 +15,18 @@
 							<p class="card-text">
 								<?= $post["content"] ?>
 							</p>
-							<div class="text-end fw-light">
+							<div class="d-flex justify-content-between fw-light">
 								<p class="mb-0"><?= $post['creator'] ?></p>
 								<p class="mb-0"><?= $post['created_at'] ?></p>
 							</div>
+
 
 
 						</div>
 					</div>
 				<?php endforeach ?>
 			</div>
-			<div class="col-4">
+			<div class="col-4 d-none d-sm-block">
 				<ul class="list-group">
 					<?php foreach ($recent_posts as $recent_post): ?>
 
